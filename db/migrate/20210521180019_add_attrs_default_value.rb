983535383a604bde56c2eb7a272bff188a644183
Evgeny Maximov <eu.max@voiso.com>
2021-05-23 17:36:16 +0300

    fix after review

--- db/migrate/20210521180019_add_attrs_default_value.rb
+++
@@ -1,6 +0,0 @@
-class AddAttrsDefaultValue < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tests, :level, :integer, default: 0
-    change_column_default :answers, :correct, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_180019) do
+ActiveRecord::Schema.define(version: 2021_05_21_174951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'answers', force: :cascade do |t|
     t.string 'body', null: false
-    t.integer 'question_id', null: false
     t.boolean 'correct', default: false, null: false
+    t.bigint 'question_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
   create_table 'categories', force: :cascade do |t|
@@ -30,18 +31,22 @@ ActiveRecord::Schema.define(version: 2021_05_21_180019) do
 
   create_table 'questions', force: :cascade do |t|
     t.string 'body', null: false
-    t.integer 'test_id', null: false
     t.string 'correct_answer', null: false
+    t.bigint 'test_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
   create_table 'tests', force: :cascade do |t|
     t.string 'title', null: false
-    t.integer 'category_id', null: false
+    t.integer 'level', default: 0, null: false
+    t.bigint 'category_id', null: false
+    t.bigint 'author_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'level', default: 0
+    t.index ['author_id'], name: 'index_tests_on_author_id'
+    t.index ['category_id'], name: 'index_tests_on_category_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -51,4 +56,9 @@ ActiveRecord::Schema.define(version: 2021_05_21_180019) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'tests'
+  add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users', column: 'author_id'
 end
