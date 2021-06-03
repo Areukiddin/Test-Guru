4d00486002f0967aceec5ece71c1ba04952a453a
Evgeny Maximov <eu.max@voiso.com>
2021-06-03 23:21:30 +0300

    associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+
   def self.titles_by(category)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+
   def results_list_by(level)
     Test.joins('LEFT JOIN results ON tests.id = results.test_id')
         .where(results: { user_id: id }, level: level)
