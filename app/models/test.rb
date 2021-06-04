35ac0956e77f414f73a35c2dec1f297d30ae1bfb
Evgeny Maximov <eu.max@voiso.com>
2021-06-04 18:53:04 +0300

    resolves

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :author, class_name: 'User', inverse_of: :user
 
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results, dependent: :destroy
 
-  def self.titles_by(category)
-    joins('JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: category })
+  def self.titles_by(category_title)
+    joins(:category)
+      .where(categories: { title: category_title })
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :results
+  has_many :results, dependent: :destroy
   has_many :tests, through: :results
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, inverse_of: :tests, dependent: :destroy
 
   def results_list_by(level)
-    Test.joins('LEFT JOIN results ON tests.id = results.test_id')
+    Test.joins(:results)
         .where(results: { user_id: id }, level: level)
   end
 end
