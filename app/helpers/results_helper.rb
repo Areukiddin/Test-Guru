467c26266b84b2008dcbe28aaeeec59083c82258
Evgeny Maximov <eu.max@voiso.com>
2021-06-21 14:13:22 +0300

    completed

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,13 @@
 module ResultsHelper
+  def questions_counter(test_passage)
+    "Question #{test_passage.test.questions.find_index(test_passage.current_question) + 1}/#{test_passage.test.questions.count}"
+  end
+
+  def correct_answers_percent(test_passage)
+    "#{(test_passage.correct_answers.to_f / test_passage.test.questions.each { |q| q.answers.correct }.count * 100).to_i}%"
+  end
+
+  def success_result?(test_passage)
+    correct_answers_percent(test_passage).to_i >= 85
+  end
 end
