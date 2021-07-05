e895cd754c11a92d56321b3f1806deecd8745959
Evgeny Maximov <eu.max@voiso.com>
2021-07-05 23:58:56 +0300

    revert

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,20 +3,3 @@
 
 const channels = require.context(".", true, /_channel\.js$/);
 channels.keys().forEach(channels);
-
-document.addEventListener("DOMContentLoaded", () => {
-  const passwordField = document.getElementById("user_password");
-  const passwordConfirmationField = document.getElementById(
-    "user_password_confirmation"
-  );
-
-  passwordConfirmationField.addEventListener("input", (e) => {
-    if (e.target.value !== passwordField?.value) {
-      passwordConfirmationField.classList.add("wrong");
-    }
-
-    if (e.target.value === passwordField?.value) {
-      passwordConfirmationField.classList.remove("wrong");
-    }
-  });
-});
