8d7b3b2de35a814207e7ded2601e416f7ceef1ad
Evgeny Maximov <eu.max@voiso.com>
2021-07-05 23:53:11 +0300

    in prog

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,22 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+const channels = require.context(".", true, /_channel\.js$/);
+channels.keys().forEach(channels);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const passwordField = document.getElementById("user_password");
+  const passwordConfirmationField = document.getElementById(
+    "user_password_confirmation"
+  );
+
+  passwordConfirmationField.addEventListener("input", (e) => {
+    if (e.target.value !== passwordField?.value) {
+      passwordConfirmationField.classList.add("wrong");
+    }
+
+    if (e.target.value === passwordField?.value) {
+      passwordConfirmationField.classList.remove("wrong");
+    }
+  });
+});
