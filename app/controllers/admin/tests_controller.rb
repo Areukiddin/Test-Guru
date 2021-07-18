df555eaa0156476b1338cd955d0c804987c3a09e
Evgeny Maximov <eu.max@voiso.com>
2021-07-18 19:08:19 +0300

    timer

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -46,7 +46,7 @@ class Admin::TestsController < Admin::BaseController
   private
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :minutes_for_passing, :category_id)
   end
 
   def find_test
